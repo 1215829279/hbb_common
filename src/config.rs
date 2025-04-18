c19bce5c0c6138f63e0c7022706bfdb1c67ddf2a
1215829279 <43942909+1215829279@users.noreply.github.com>
2025-04-18 14:41:03 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["nh.1215829279.top"];
+pub const RS_PUB_KEY: &str = "OyLEdCfXurU7KQJI6BZFZU9Bq1K5h93672a3SXzEaJE=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
